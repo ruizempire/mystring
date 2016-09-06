8a3070af0a4cf4d6e17b3400904a29d022d82def
ruizempire <ruizjm@vcu.edu>
2016-09-06 18:37:56 -0400

    test

--- mystring/mystring.cpp
+++ mystring/mystring.cpp
@@ -2,10 +2,17 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
 
 
 int main()
 {
+	char name[50];
+	cout << "enter a name" << endl;
+	gets_s(name);
+	cout << "your name is" << name << endl;
     return 0;
 }
 
